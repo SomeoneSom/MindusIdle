a1459735c5c68aec8169a6c6c12a010b4a8dad83
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-12-17 10:30:39 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ var divs = ["res", "shop", "upg", "land"]
 function enableDiv(divname) {
   for (var j = 0; j < divs.length; j++) {
     var divtag = divs[j]
-    var nodes = document.getElementById(divtag).getElementsByTagName('*');
-    for(var i = 0; i < nodes.length; i++){
-      if (divtag == divname) {
-        nodes[i].disabled = false;
-      } else {
-        nodes[i].disabled = true;
-      }
+    var div = document.getElementById(divtag)
+    if (divname == divtag) {
+      div.style.display = "block"
+    } else {
+      div.style.display = "none"
     }
   }
 }
