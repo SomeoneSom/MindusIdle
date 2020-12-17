ae5f0ee7a80944146f7fca4dd04789d00571c2ed
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-12-17 10:14:26 -0800

    navbar func

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-
+var divs = ["res", "shop", "upg", "land"]
+function enableDiv(divname) {
+  for (var j = 0; j < divs.length; j++) {
+    var divtag = divs[j]
+    var nodes = document.getElementById(divtag).getElementsByTagName('*');
+    for(var i = 0; i < nodes.length; i++){
+      if (divtag == divname) {
+        nodes[i].disabled = false;
+      } else {
+        nodes[i].disabled = true;
+      }
+    }
+  }
+}
+enableDiv("res")
