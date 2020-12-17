db929efa5817aad2dd717a4b9aaf90ee1f0fcf95
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-12-17 10:32:57 -0800

    somehow forgot to add semicolons

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var divs = ["res", "shop", "upg", "land"]
+var divs = ["res", "shop", "upg", "land"];
 function enableDiv(divname) {
   for (var j = 0; j < divs.length; j++) {
-    var divtag = divs[j]
-    var div = document.getElementById(divtag)
+    var divtag = divs[j];
+    var div = document.getElementById(divtag);
     if (divname == divtag) {
-      div.style.display = "block"
+      div.style.display = "block";
     } else {
-      div.style.display = "none"
+      div.style.display = "none";
     }
   }
 }
-enableDiv("res")
+enableDiv("res");
