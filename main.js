5c1067a9e8238e22710aac404ac2bbfa064efa06
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-12-19 20:14:20 -0800

    no more hyperlink info

--- main.js
+++ main.js
@@ -59,3 +59,11 @@ window.setInterval(function(){
 window.setInterval(function(){
   document.getElementById("copperres").innerHTML = resources.copper.toString(10);
 }, 100);
+var aTags = document.querySelectorAll('span[data-href]');
+for(var i = 0; i < aTags.length; i++) {
+  var aTag = aTags[i];
+  aTag.addEventListener('click', function(e) {
+    var ele = e.target;
+    window.location.replace(ele.getAttribute('data-href'));
+  });    
+}
