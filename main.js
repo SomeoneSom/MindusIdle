57faae4b273a761a161655277af2fc5418e93a50
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2020-12-24 20:03:21 -0800

    resource output

--- main.js
+++ main.js
@@ -31,8 +31,8 @@ enableDiv("res", divsmain);
 colorA("res", divsmain);
 //obj format: [count, costres, costdrills, output]
 function wipeSave() {
-  window.localStorage['resources'] = JSON.stringify({"copper":30, "lead":0, "scrap":0, "sand":0, "graphite":0, "metaglass":0, "spore_pod":0, "coal":0, "titanium":0, "thorium":0, "silicon":0, "plastanium":0, "phase_fabric":0, "surge_alloy":0, "blast_compound":0, "pyratite":0, "land":10});
-  window.localStorage['obj'] = JSON.stringify({"mech":[0, [30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]});
+  window.localStorage['resources'] = JSON.stringify({"copper":25, "lead":0, "scrap":0, "sand":0, "graphite":0, "metaglass":0, "spore_pod":0, "coal":0, "titanium":0, "thorium":0, "silicon":0, "plastanium":0, "phase_fabric":0, "surge_alloy":0, "blast_compound":0, "pyratite":0, "land":10});
+  window.localStorage['obj'] = JSON.stringify({"mech":[0, [25, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]});
 }
 function loadSave() {
   resources = JSON.parse(window.localStorage['resources']);
@@ -79,9 +79,14 @@ window.setInterval(function(){
   for (var i = 0; i < Object.keys(obj).length; i++) {
     document.getElementById(i.toString(10).concat("count")).innerHTML = obj[Object.keys(obj)[i]][0].toString(10);
     for (var j = 0; j < 17; j++) {
+      out = obj[Object.keys(obj)[i]][2][j];
       cost = obj[Object.keys(obj)[i]][1][j].toString(10);
       if (cost != "0") {
         document.getElementById(i.toString(10).concat("cost", j.toString(10))).innerHTML = cost;
+      }
+      if (out != 0) {
+        out *= obj[Object.keys(obj)[i]][0];
+        document.getElementById(i.toString(10).concat("out", j.toString(10))).innerHTML = out.toString(10); 
       }
     }
   }
