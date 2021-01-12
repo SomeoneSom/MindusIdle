3e4a58cb66530879b1f610e6fe87457409e38a7a
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2021-01-12 09:15:45 -0800

    Added combustion generators (most likely buggy)

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ var divsmain = ["res", "drshop", "crshop", "upg", "power", "land", "options"];
 var divsdr = ["mech", "leadr", "coaldr"];
 var divscr = ["graphpress"];
 var divslnd = ["duo"];
-var divspower = [];
+var divspower = ["combusgen"];
 var resources;
 var obj;
 function enableDiv(divname, divs) {
